7bf147696a412350c3df7f32222ec4ccdaacb318
Brandon C. Kelly <bckelly80@gmail.com>
2013-10-28 16:18:27 -0700

    Resurrected old python MCMC classes for numba

---
+++ priors.py
@@ -0,0 +1,104 @@
+"""
+This file contains the class definitions for various priors.
+"""
+
+__author__ = 'Brandon C. Kelly'
+
+import numpy as np
+from scipy import stats
+
+
+class Prior(object):
+    """
+    Base class for prior distribution objects. The two main methods of the prior class are Draw() and
+    LogDensity(), both of which must be overriden in order to be used. However, it is only required that
+    LogDensity be overriden, as it is the only method required by an MCMC sampler.
+
+    This class is merely a base class and should never be instantiated.
+    """
+
+    def __init__(self, temperature=1.0):
+        """
+        Constructor for prior object.
+
+        :param temperature: The prior 'temperature', used for tempered distributions. If you don't
+                            know what this means than just use the default of 1.0, since this corresponds
+                            to "normal" MCMC.
+        """
+        self.temperature = temperature
+
+    def LogDensity(self, value):
+        """
+        The logarithm of the prior distribution evaluated at the input value.
+
+        :param value: Compute the prior distribution at this value.
+        """
+        return 0.0
+
+    def Draw(self):
+        """
+        Return a random draw from the prior distribution.
+        """
+        return 0.0
+
+
+class Uninformative(Prior):
+    """
+    An uninformative prior. This just return 0.0 for the logarithm of the prior distribution for all
+    values of the parameter. The Draw() method is undefined for this class and should not be used.
+    """
+
+    def LogDensity(self, value):
+        return 0.0
+
+
+class Normal(Prior):
+    """
+    A normal prior.
+    """
+
+    def __init__(self, mu, variance, temperature=1.0):
+        """
+        Constructor for normal prior object.
+
+        :param mu: The prior mean.
+        :param variance: The prior variance.
+        :param temperature: Temperature of prior distribution, see Prior class documentation.
+        """
+        Prior.__init__(self, temperature=temperature)
+        self.mu = mu
+        self.variance = variance
+        self.sigma = np.sqrt(variance)
+
+    def LogDensity(self, value):
+        return stats.norm.logpdf(value, loc=self.mu, scale=self.sigma)
+
+    def Draw(self):
+        return stats.norm.rvs(loc=self.mu, scale=self.sigma)
+
+
+class ScaledInvChiSqr(Prior):
+    """
+    A scaled inverse-chi-square prior object.
+    """
+
+    def __init__(self, dof, ssqr, temperature=1.0):
+        """
+        Constructor for scaled inverse-chi-square prior object.
+
+        :param dof: The prior degrees of freedom.
+        :param ssqr: The prior variance.
+        :param temperature: Temperature of prior distribution, see Prior class documentation.
+        """
+        Prior.__init__(self, temperature=temperature)
+        assert dof > 0  # Make sure parameter values are positive
+        assert ssqr > 0
+        self.dof = dof
+        self.ssqr = ssqr
+
+    def LogDensity(self, value):
+        assert value > 0
+        return stats.invgamma.logpdf(value, self.dof / 2.0, scale=self.dof * self.ssqr / 2.0)
+
+    def Draw(self):
+        return stats.invgamma.rvs(self.dof / 2.0, scale=self.dof * self.ssqr / 2.0)
---
+++ proposals.py
@@ -0,0 +1,128 @@
+"""
+This file contains the class definitions for proposal objects. In general, these are needed by any Metropolis-Hastings
+steps (the class definitions for M-H steps are given in steps.py). In order to use the M-H step and its subclasses one
+must supply a proposal object.
+"""
+
+import numpy as np
+import math
+
+__author__ = "Brandon C. Kelly"
+
+
+class Proposal(object):
+    """
+    Base proposal class. This is an empty class and is just a template for proposal classes derived from this
+    class. The methods of Proposal should be overridden in the derived classes.
+    """
+
+    def Draw(self, current_value):
+        """
+        Method to generate a proposal given the current value of the parameter.
+
+        :param current_value: The current value of the parameter.
+        """
+        pass
+
+    def LogDensity(self, proposed_value, current_value):
+        """
+        Method to return the logarithm of the probability of going from the current value to the proposed value
+        of the parameter.
+
+        :param proposed_value: The proposed value of the parameter.
+        :param current_value:  The current value of the parameter.
+        """
+        pass
+
+
+class NormalProposal(Proposal):
+    """
+    Generate proposals from a univariate Normal distribution centered at the current value of the parameter.
+    """
+
+    def __init__(self, sigma):
+        """
+        Constructor for Normal Proposal object.
+
+        :param sigma: The standard deviation of the proposed values.
+        """
+        self.sigma = sigma
+
+    def Draw(self, current_value):
+        proposed_value = np.random.normal(current_value, self.sigma)
+        return proposed_value
+
+    def LogDensity(self, proposed_value, current_value):
+        return 0.0  # Symmetric proposal, so just return zero.
+
+
+class MultiNormalProposal(Proposal):
+    """
+    Generate proposals from a multivariate normal distribution centered at the current value of the parameter.
+    """
+
+    def __init__(self, covar):
+        """
+        Constructor for multivariate normal proposal object.
+
+        :param covar (array-like): The covariance matrix of the proposals.
+        """
+        self.covar = covar
+
+    def Draw(self, current_value):
+        proposed_value = np.random.multivariate_normal(current_value, self.covar)
+        return proposed_value
+
+    def LogDensity(self, proposed_value, current_value):
+        return 0.0  # Symmetric proposal, so returned value doesn't matter.
+
+
+class LogNormalProposal(Proposal):
+    """
+    Generate proposals from a log-normal distribution.
+    """
+
+    def __init__(self, scale):
+        """
+        Constructor for the Log-normal proposal object.
+
+        :param scale: The standard deviation of log(parameter value)
+        """
+        self.scale = scale
+
+    def Draw(self, current_value):
+        assert current_value > 0  # Make sure values are positive
+        proposed_value = np.random.lognormal(math.log(current_value), self.scale)
+        return proposed_value
+
+    def LogDensity(self, proposed_value, current_value):
+        assert current_value > 0  # Make sure values are positive
+        assert proposed_value > 0
+        chi = (math.log(proposed_value) - math.log(current_value)) / self.scale
+        log_density = -math.log(proposed_value) - chi * chi
+        return log_density
+
+
+class StudentProposal(Proposal):
+    """
+    Generate proposals from a Student's t-distribution.
+    """
+
+    def __init__(self, dof, scale):
+        """
+        Constructor for the Student's t-distribution proposal object.
+
+        :param dof: The degrees of freedom for the t-distribution proposals.
+        :param scale: The scale of the t-distribution.
+        """
+        self.dof = dof
+        self.scale = scale
+
+    def Draw(self, current_value):
+        proposed_value = current_value + self.scale * np.random.standard_t(self.dof)
+        return proposed_value
+
+    def LogDensity(self, proposed_value, current_value):
+        return 0.0  # Symmetric proposal, so returned value does not matter.
+
+
