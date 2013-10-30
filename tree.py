9dd275cd195e7af9bf1e14c4bd3872aa33f57a8d
Brandon C. Kelly <bckelly80@gmail.com>
2013-10-30 15:31:46 -0700

    fixed CartTree.loglik(), added method to compute depth of a node to Node.

--- tree.py
+++ tree.py
@@ -274,6 +274,8 @@ class CartTree(BaseTree):
         self.lamb  = lamb
         self.mubar = mubar
         self.a     = a
+        self.alpha = alpha
+        self.beta = beta
 
         # Build the tree
         self.buildUniform(self.head, alpha, beta)
@@ -296,39 +298,53 @@ class CartTree(BaseTree):
         else:
             print "NOT SPLITTING node", node.Id, ": did not pass random draw"
 
+    def logprior(self, tree):
+
+        logprior = 0.0
+        # first get prior for terminal nodes
+        for node in tree.terminalNodes:
+            logprior += np.log(1.0 - self.alpha / (1.0 + depth) ** self.beta)
+
+            fxl, fyl = tree.filter(node)
+            npts = np.sum(fyl)
+            nfeatures = np.sum(fxl, axis=0)
+
     # NOTE: This part would likely benefit from numba or cython
-    def regressionLnlike(self):
+    def loglik(self, tree):
+        """
+        Compute the log-likelihood for a proposed tree model. This assumes that the only difference between the input
+        tree and self is in the structure of the tree nodes. The prior and data are assumed to be the same.
+
+        @param tree: The proposed tree.
+        @return: The log-likelihood of tree.
+        """
         lnlike = 0.0
 
         # Precalculate terms
         t2  = np.log((self.nu * self.lamb)**(0.5 * self.nu))
         t4b = gammaln(0.5 * self.nu)
         
-        for node in self.terminalNodes:
-            fxl, fyl = self.filter(node)
-            npts    = np.sum(fyl)
+        for node in tree.terminalNodes:
+            fxl, fyl = tree.filter(node)
+            npts = np.sum(fyl)
             if npts == 0:
                 # Damn, this should not happen.
                 # DEBUG ME
                 continue
-            ymean   = np.mean(self.y[fyl])
-            ystd    = np.std(self.y[fyl])
-
-            # Random draws for mean-variance shift model
-            sigsq   = stats.invgamma.rvs(0.5 * self.nu, scale = 0.5 * self.nu * self.lamb)
-            mui     = stats.norm.rvs(self.mubar, scale = sigsq / self.a)
+            ymean = np.mean(self.y[fyl])
+            yvar = np.var(self.y[fyl], ddof=1)
 
             # Terms that depend on the data moments
-            si      = (npts - 1) * ystd
-            ti      = (npts * self.a) / (npts + self.a) * (ymean - self.mubar)**2
+            si = (npts - 1) * yvar
+            ti = (npts * self.a) / (npts + self.a) * (ymean - self.mubar)**2
 
             # Calculation of the log likelihood (Chipman Eq 14)
-            t1      = -0.5 * npts * np.log(np.pi)
-            t3      = +0.5 * np.log(self.a / (npts + self.a))
-            t4      = gammaln(0.5 * (npts + self.nu)) - t4b
-            t5      = -0.5 * (npts + self.nu) * np.log(si + ti + self.nu * self.lamb)
+            t1 = -0.5 * npts * np.log(np.pi)
+            t3 = +0.5 * np.log(self.a / (npts + self.a))
+            t4 = gammaln(0.5 * (npts + self.nu)) - t4b
+            t5 = -0.5 * (npts + self.nu) * np.log(si + ti + self.nu * self.lamb)
             lnlike += t1 + t2 + t3 + t4 + t5
-            #print npts, ymean, ystd, lnlike
+            #print npts, ymean, yvar, lnlike
 
         return lnlike
 
@@ -355,6 +371,18 @@ class Node(object):
     def setThreshold(self, feature, threshold):
         self.feature = feature
         self.threshold = threshold
+
+    def find_depth(self):
+        """
+        Find the depth of this node by moving back through the parents until we reach the base of the tree.
+        """
+        depth = 0
+        parent = self.Parent
+        while parent is not None:
+            parent = parent.Parent
+            depth += 1
+
+        return depth
 
 if __name__ == "__main__":
     nsamples  = 1000
